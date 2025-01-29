7cd85ef9f1ae41f42cd9188f73c6e252f7f1b363
Anton <toropow@gmail.com>
2025-01-29 22:32:34 +0300

    add model

--- src/main/java/com/anton/spring/mvc/myController.java
+++ src/main/java/com/anton/spring/mvc/myController.java
@@ -3,10 +3,12 @@ package com.anton.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class myController {
     @RequestMapping("/")
     public String showFirstView(){
@@ -23,9 +25,16 @@ public class myController {
 //        return "show-emp-details-view";
 //    }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model){
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model){
-        String empName = request.getParameter("employeeName");
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model){
         empName = "Mr. " + empName;
         model.addAttribute("nameAttribute", empName);
         return "show-emp-details-view";
